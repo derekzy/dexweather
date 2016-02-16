515cded00e9c3185f2091ba318aa39ed8ce66e55
derekzy <136618962@qq.com>
2016-02-16 12:51:25 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/derekzy/dexweather/db/DexWeatherDB.java
@@ -0,0 +1,118 @@
+package com.derekzy.dexweather.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.derekzy.dexweather.model.City;
+import com.derekzy.dexweather.model.County;
+import com.derekzy.dexweather.model.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by derekzy on 2016/2/16.
+ */
+public class DexWeatherDB {
+    /**
+     *  原本我们需要在Activity中实例化DatabaseOpenHelper，然后把数据put进去，我们在这里进行封装方法
+     */
+
+    public static final String DB_NAME = "dex_weather";
+
+    public static final int VERSION = 1;
+    private static DexWeatherDB dexWeatherDB;
+    private SQLiteDatabase db;
+
+    private DexWeatherDB(Context context) {
+        DexWeatherOpenHelper dbHelper = new DexWeatherOpenHelper(context, DB_NAME, null, VERSION);
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public synchronized static DexWeatherDB getInstance(Context context) {
+        if(dexWeatherDB == null) {
+            dexWeatherDB = new DexWeatherDB(context);
+        }
+        return dexWeatherDB;
+    }
+
+    public void saveProvince(Province province) {
+        if (province != null) {
+            ContentValues values = new ContentValues();
+            values.put("province_name", province.getProvinceName());
+            values.put("province_code", province.getProvinceCode());
+            db.insert("Province", null, values);
+        }
+    }
+
+    public List<Province> loadProvinces() {
+        List<Province> provinceList = new ArrayList<>();
+        Cursor cursor = db.query("Province", null, null, null, null, null, null);
+        if (cursor.moveToNext()) {
+            Province province = new Province();
+            province.setId(cursor.getInt(cursor.getColumnIndex("_id")));
+            province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+            province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+            provinceList.add(province);
+        }
+        else if (cursor != null) {
+            cursor.close();
+        }
+        return provinceList;
+    }
+
+    public void saveCity(City city) {
+        if (city != null) {
+            ContentValues values = new ContentValues();
+            values.put("city_name", city.getCityName());
+            values.put("city_code", city.getCityCode());
+            values.put("province_id", city.getProvinceId());
+            db.insert("City", null, values);
+        }
+    }
+
+    public List<City> loadCities(int provinceId) {
+        List<City> cityList = new ArrayList<>();
+        Cursor cursor = db.query("City", null, "province_id = ?", new String[] {String.valueOf(provinceId)}, null, null, null);
+        if (cursor.moveToNext()) {
+            City city = new City();
+            city.setId(cursor.getInt(cursor.getColumnIndex("_id")));
+            city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+            city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+            city.setProvinceId(provinceId);
+            cityList.add(city);
+        }
+        else if (cursor != null) {
+            cursor.close();
+        }
+        return cityList;
+    }
+
+    public void saveCounty(County county) {
+        if (county != null) {
+            ContentValues values = new ContentValues();
+            values.put("county_name", county.getCountyName());
+            values.put("county_code", county.getCountyCode());
+            values.put("City_id", county.getCityId());
+            db.insert("County", null, values);
+        }
+    }
+
+    public List<County> loadCounties(int cityId) {
+        List<County> countyList = new ArrayList<>();
+        Cursor cursor = db.query("County", null, "city_id = ?", new String[]{String.valueOf(cityId)}, null, null, null);
+        if (cursor.moveToNext()) {
+            County county = new County();
+            county.setId(cursor.getInt(cursor.getColumnIndex("_id")));
+            county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+            county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+            county.setCityId(cityId);
+            countyList.add(county);
+        } else if (cursor != null) {
+            cursor.close();
+        }
+        return countyList;
+    }
+}
---
+++ app/src/main/java/com/derekzy/dexweather/db/DexWeatherOpenHelper.java
@@ -0,0 +1,33 @@
+package com.derekzy.dexweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by derekzy on 2016/2/16.
+ */
+public class DexWeatherOpenHelper extends SQLiteOpenHelper {
+    public DexWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table if not exists Province(_id integer primary key autoincrement," +
+                " province_name text not null," +
+                "province_code text not null)");
+        db.execSQL("create table if not exists City(_id integer primary key autoincrement," +
+                " city_name_name text not null," +
+                " city_code text not null, province_id integer not null)");
+        db.execSQL("create table if not exists County(_id integer primary key autoincrement," +
+                " county_name text not null," +
+                " province_code text not null, city_id integer not null)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
